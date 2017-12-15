b6b5b26dd83f42c2237a567b4708766382b5eddd
ferdy26790 <ferdy26790@gmail.com>
2017-12-15 14:08:05 +0700

    addlink

--- server/routes/dummy.js
+++ server/routes/dummy.js
@@ -10,8 +10,12 @@ router.post('/', function(req, res, next) {
   console.log(req.headers.token);
   FB.setAccessToken(req.headers.token);
 
-  var body = 'https://www.google.com';
-  FB.api('me/feed', 'post', { message: body }, function (res) {
+  let body = 'http://www.mtv.com/news/3053026/finn-wolfhard-stranger-things-it-google-most-searched-2017/';
+  let picUrl = 'https://imagesmtv-a.akamaihd.net/uri/mgid:ao:image:mtv.com:260093?height=729&width=1296&format=jpg&quality=.7'
+  FB.api('me/feed', 'post', {
+     link: body,
+     picture: picUrl
+   }, function (res) {
     if(!res || res.error) {
       console.log(!res ? 'error occurred' : res.error);
       return;
