5ae2e40ce6e5ce19554019e1313628a54f18ee21
wahibhanii <wahib.hanii@gmail.com>
2017-12-15 08:39:42 +0700

    add addUserNews

--- server/models/news.js
+++ server/models/news.js
@@ -1,12 +1,19 @@
 'use strict'
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://wahibhacktiv8:${process.env.DB_PASSWORD}@wahib-hacktiv8-shard-00-00-uyl7c.mongodb.net:27017,wahib-hacktiv8-shard-00-01-uyl7c.mongodb.net:27017,wahib-hacktiv8-shard-00-02-uyl7c.mongodb.net:27017/test?ssl=true&replicaSet=wahib-hacktiv8-shard-0&authSource=admin`);
+mongoose.connect(`mongodb://wahibhacktiv8:${process.env.DB_PASSWORD}@wahib-hacktiv8-shard-00-00-uyl7c.mongodb.net:27017,wahib-hacktiv8-shard-00-01-uyl7c.mongodb.net:27017,wahib-hacktiv8-shard-00-02-uyl7c.mongodb.net:27017/newsifiy?ssl=true&replicaSet=wahib-hacktiv8-shard-0&authSource=admin`,{ useMongoClient: true });
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  data: Schema.Types.Mixed
+const newsSchema = new Schema({
+  source      : Schema.Types.Mixed,
+  author      : String,
+  title       : String,
+  description : String,
+  url         : String,
+  urlToImage  : String,
+  publishedAt : Date
 });
 
-const User = mongoose.model('Users', userSchema);
 
-module.exports = User;
+const News = mongoose.model('News', newsSchema);
+
+module.exports = News;
